d39dc41751e68e3260eecca26c984f2dbe2ab232
Junior Saul Maiz Echevarria <junior.maiz@unas.edu.pe>
2022-06-07 15:11:32 -0500

    creación del contenido de los controladores: franco, generar pdf, home, HoraExtra, PagoIncentivo, Pdf, SalidasParticulares, TipoAusencia, Vacaciones y VacacionesEdition

--- app/Http/Controllers/FrancoController.php
+++ app/Http/Controllers/FrancoController.php
@@ -3,82 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Empleado;
+use App\Franco;
 class FrancoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function francos()
     {
-        //
+        $empleados = Empleado::where('id', 1)->get();
+        $francos = Franco::with('empleado')->get();
+        return view('franco.lista', [
+            'francos' => $francos,
+            'empleados' => $empleados,
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function agregarfrancos()
     {
-        //
+        $empleados = Empleado::get();
+        return view('franco.add', ['empleados' => $empleados]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function agregarfrancosid($id)
     {
-        //
+        $empleados = Empleado::all();
+        $empleado = Empleado::where('id', $id)->first();
+        return view('franco.addfromempleado', [
+            'empleado' => $empleado,
+            'empleados' => $empleados,
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function guardarfrancos(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $empleados = Empleado::all();
+        $franco = Franco::create($request->all());
+        $francos = Franco::with('empleado')->get();
+        return view('franco.lista', [
+            'francos' => $francos,
+            'empleados' => $empleados,
+        ]);
     }
 }
